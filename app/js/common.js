222387d9133a8c9f396c911561c36b21617299cd
Sergey <code.sergey.dolinin@gmail.com>
2017-09-19 01:22:43 +0300

    ---

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,16 @@
 $(function() {
 
-	$(".owl-carousel").owlCarousel({
-		loop: true,
-		items: 1,
-		nav: true,
-	});
+		$(".owl-carousel").owlCarousel({
+				loop: true,
+				items: 1,
+				nav: true,
+		});
 
+		$(".toggle-mnu").click(function() {
+  			$(this).toggleClass("on");
+  			$(".menu-2").slideToggle();
+  			return false;
+		});
+
+    
 });
