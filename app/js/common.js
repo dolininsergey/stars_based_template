149200d0e8c0136728f72248bada7657969cd04b
Sergey <code.sergey.dolinin@gmail.com>
2017-09-21 22:59:49 +0300

    ---

--- app/js/common.js
+++ app/js/common.js
@@ -1,9 +1,15 @@
+
+
+
+
+
 $(function() {
 
 		$(".owl-carousel").owlCarousel({
 				loop: true,
 				items: 1,
 				nav: true,
+				navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
 		});
 
 		$(".toggle-mnu").click(function() {
@@ -12,5 +18,18 @@ $(function() {
   			return false;
 		});
 
-    
+
+// var shortMenu 	 = document.querySelector('.menu-1');
+// var searchButton = document.querySelector('.fa-search');
+// var searchField  = document.querySelector('.search-field');
+
+// searchButton.onclick() = function() {
+// 	// searchField = style.display = 'none';
+// 	alert("Hello!");
+// }
+
+$('.search-button').click(function(e) {
+		$(this).css('color', '#1A54E7'); 
+  	$('.menu-1 input').animate({width: 'toggle'});
+	});    
 });
